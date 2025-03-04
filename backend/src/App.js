537ed6cb6cf0a1147a4fcb38595941a19b21c233
Amanda Smith <amanda.smith@auib.edu.iq>
2025-03-04 18:36:51 +0300

    Fix: Removed backend as a submodule and added it as a normal folder

---
+++ backend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
